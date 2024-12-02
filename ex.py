8ee6a4ec74b79508878eb2c6eb5dd9edb4a3c7de
dain.kim <dain9908@naver.com>
2024-12-02 19:21:29 +0900

    내 입맛대로 변경해보기

--- ex.py
+++ ex.py
@@ -1,16 +1,3 @@
-'''
-l = [[1, 10, 'leehojun'], 
-     [20, 30, 'hojun'], 
-     [10, 20, 'weniv!'], 
-     [1, 2, 'hello world'], 
-     [55, 11, 'sun']]
-
-# 1. 글자 수 대로 정렬해주세요.
-
-# 2. 맨 앞에 위치한 숫자대로 정렬해주세요.
-
-# 3. 중앙에 위치한 값대로 정렬해주세요.
-'''
 
 l = [[1, 10, 'leehojun'], 
      [20, 30, 'hojun'], 
@@ -18,43 +5,55 @@ l = [[1, 10, 'leehojun'],
      [1, 2, 'hello world'], 
      [55, 11, 'sun']]
 
+
+
 # 1. 글자 수 대로 정렬해주세요.
 
-def sortLength(p):
-    return len(p[2])
+def sortLength(a):
+    return len(a[2])
 
 result_1 = sorted(l, key = sortLength)
-print("1. 글자 수 대로 정렬 : ")
-print(result_1)
+print(f"1. 글자 수 대로 정렬할 때\n{result_1}")
 
 
 print()
+
+
 
 # 2. 맨 앞에 위치한 숫자대로 정렬해주세요.
 
-def sortFirst(u):
-    return u[0]
+def sortFirstnum(b):
+    return b[0]
 
-result_2 = sorted(l, key = sortFirst)
-print("2. 맨 앞에 위치한 숫자대로 정렬 : ")
-print(result_2)
+result_2 = sorted(l,key = sortFirstnum)
+print("2. 맨 앞에 위치한 숫자대로 정렬하면 ?\n{}".format(result_2))
 
 
 print()
+
+
 
 # 3. 중앙에 위치한 값대로 정렬해주세요.
 
-def sortMiddle(n):
-    return n[1]
+def sortMiddle(c):
+    return c[1]
 
-result_3 = sorted(l, key = sortMiddle)
-
-print("3. 중앙에 위치한 값대로 정렬 : ")
-print(result_3)
-
-
+return_3 = sorted(l, key = sortMiddle)
+print(f"3. 중앙에 위치한 값대로 정렬한다면 ? \n{return_3}")
 
 print()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 i = [[1, 10, 32], 
@@ -66,7 +65,8 @@ i = [[1, 10, 32],
 
 # 4. 3개의 전체 합이 작은 순서대로 출력해주세요.
 
-def listSumFunction(s):
-    return sum(s)
+def listSumSort(d):
+    return int(d[0] + d[1] + d[2])
 
-print("3개의 요소 전체 합이 작은 순서대로 정렬 : ", sorted(i, key = listSumFunction))
+result_4 = sorted(i, key = listSumSort)
+print(f"3 개의 요소 전체 합이 작은 순서대로 정렬하면 \n{result_4}")
